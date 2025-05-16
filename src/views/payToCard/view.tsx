00e970c7df7d6cf3531571ffce2e371205127100
Tekhronkhudaykulov <tekhronkhudaykulov@gmail.com>
2025-05-16 05:37:49 +0500

    fix

--- src/views/payToCard/view.tsx
+++ src/views/payToCard/view.tsx
@@ -1,4 +1,5 @@
-import { FooterNav, Text } from "../../components"
+import { motion } from "framer-motion";
+import { FooterNav, Text } from "../../components";
 import NumericKeyboardCustom from "../../components/CustomKeyboard/NumericKeyboard";
 import { useFormContext } from "../../context/inputTypeContext";
 import InputMask from "react-input-mask";
@@ -16,122 +17,114 @@ const formatPrice = (value) => {
 };
 
 const PayToCard = () => {
-    const navigate = useNavigate()
-    // @ts-ignore
-    const { responseData, form, setActiveInputKey,setResponseData,clearForm,setError,error } = useFormContext();
+  const navigate = useNavigate();
+  // @ts-ignore
+  const { responseData, form, setActiveInputKey, setResponseData, clearForm, setError, error } = useFormContext();
 
+  const card_num = form["card_number"]?.replace(/\s/g, "").length;
+  const card_expire = form["card_expire"]?.length;
 
-    
-    
-    const card_num = form["card_number"]?.replace(/\s/g, "").length 
-    const card_expire = form["card_expire"]?.length
-
-    console.log(card_expire, 'card');
-    
-
-    
-    
-    
-
-    const { mutate, isPending, isError } = useCustomPost({
+  const { mutate, isPending, isError } = useCustomPost({
     onSuccess: async (res: any) => {
       setResponseData(res);
       await clearForm();
-      navigate(
-        `${APP_ROUTES.PAY_CARD_CONFIRM}`
-      );
+      navigate(`${APP_ROUTES.PAY_CARD_CONFIRM}`);
     },
     onError: (err) => {
       setError(err?.response?.data?.message);
     },
-    });
+  });
 
+  const { amount, ...payFormWithoutAmount } = form;
 
-    const { amount, ...payFormWithoutAmount } = form;
-
-    console.log(payFormWithoutAmount, 'pay');
-    
-
-    console.log(amount, 'amount');
-    
-    const handleSubmit = () => {
-        mutate({
-        endpoint: endpoints.cardPrepare,
-        body: {
-            vendor_form: {
-                ...responseData?.vendor_form,
-                static_amount: amount
-            },
-            pay_form: payFormWithoutAmount
+  const handleSubmit = () => {
+    mutate({
+      endpoint: endpoints.cardPrepare,
+      body: {
+        vendor_form: {
+          ...responseData?.vendor_form,
+          static_amount: amount,
         },
-        });
-    };
+        pay_form: payFormWithoutAmount,
+      },
+    });
+  };
 
-    
+  return (
+    <>
+      {isError && <Notification message={error} onClose="" />}
+      {isPending && <LoadingPage />}
 
-    return (
-     <>
-        {isError && (
-            <Notification message={error} onClose=""/>
-        )}
-        {isPending && (
-            <LoadingPage/>
-        )}
-            <div className="grid  grid-cols-[65%_1fr] gap-x-[20px]">
-                <div className="bg-[#F4F4F4] flex flex-col justify-between rounded-[12px] gap-y-[20px] w-full p-[30px]">
-                    <div className="bg-white  flex justify-between items-center p-[20px]  rounded-[12px]">
-                        <img
-                            src={responseData?.vendor?.logo}
-                            className="min-w-[88px] w-[88px] h-[88px] rounded-full object-contain"
-                            alt=""
-                        />
-                        <Text
-                            text={`${responseData?.vendor?.name}`}
-                            className="text-[25px] font-bold"
-                        />
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <label htmlFor="card-number" className="text-[30px] mb-[5px] text-gray-700 font-[500]">
-                    *Сумма
-                        </label>
-                    <input id="amount"  type="text"  value={formatPrice(form["amount"] || "")}  onFocus={() => setActiveInputKey("amount")}  className="p-3 bg-white h-[75px] rounded-[12px] text-[27px]" />
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <label htmlFor="card-number" className="text-[30px] mb-[5px] text-gray-700 font-[500]">
-                    *Введите номер карты
-                        </label>
-                        <InputMask
-                        id="card_number"
-                        mask="9999 9999 9999 9999"
-                        value={form["card_number"] || ""}
-                        onFocus={() => setActiveInputKey("card_number")}
-                        placeholder="____ ____ ____ ____"
-                        className="p-3 bg-white h-[75px] rounded-[12px] text-[27px]"
-                        />
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <label htmlFor="expiry" className="text-[30px] mb-[5px] text-gray-700 font-[500]">
-                        *Введите срок карты
-                        </label>
-                        <InputMask
-                        id="card_expire"
-                        mask="99/99"
-                        placeholder="MM/YY"
-                        className="p-3 h-[75px] rounded-[12px] text-[27px]"
-                        value={form["card_expire"] || ""}
-                        onFocus={() => setActiveInputKey("card_expire")}
-                        />
-                    </div>
-                </div>
-                <div className="bg-[#F4F4F4] rounded-[12px]">
-                    <NumericKeyboardCustom/>
-                </div>
-            </div>
-            <div className="mt-auto">
-                <FooterNav nextTitle="Получить СМС-код" nextClick={() => handleSubmit()} nextDisabled={card_num  !== 16 || card_expire !== 4}/>
-            </div>
-     </>
-    )
-}
+      {/* Motion container */}
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        className="grid grid-cols-[65%_1fr] gap-x-[20px]"
+      >
+        <div className="bg-[#F4F4F4] flex flex-col justify-between rounded-[12px] gap-y-[20px] w-full p-[30px]">
+          <div className="bg-white flex justify-between items-center p-[20px] rounded-[12px]">
+            <img
+              src={responseData?.vendor?.logo}
+              className="min-w-[88px] w-[88px] h-[88px] rounded-full object-contain"
+              alt=""
+            />
+            <Text text={`${responseData?.vendor?.name}`} className="text-[25px] font-bold" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="card-number" className="text-[30px] mb-[5px] text-gray-700 font-[500]">
+              *Сумма
+            </label>
+            <input
+              id="amount"
+              type="text"
+              value={formatPrice(form["amount"] || "")}
+              onFocus={() => setActiveInputKey("amount")}
+              className="p-3 bg-white h-[75px] rounded-[12px] text-[27px]"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="card-number" className="text-[30px] mb-[5px] text-gray-700 font-[500]">
+              *Введите номер карты
+            </label>
+            <InputMask
+              id="card_number"
+              mask="9999 9999 9999 9999"
+              value={form["card_number"] || ""}
+              onFocus={() => setActiveInputKey("card_number")}
+              placeholder="____ ____ ____ ____"
+              className="p-3 bg-white h-[75px] rounded-[12px] text-[27px]"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="expiry" className="text-[30px] mb-[5px] text-gray-700 font-[500]">
+              *Введите срок карты
+            </label>
+            <InputMask
+              id="card_expire"
+              mask="99/99"
+              placeholder="MM/YY"
+              className="p-3 h-[75px] rounded-[12px] text-[27px]"
+              value={form["card_expire"] || ""}
+              onFocus={() => setActiveInputKey("card_expire")}
+            />
+          </div>
+        </div>
+        <div className="bg-[#F4F4F4] rounded-[12px]">
+          <NumericKeyboardCustom />
+        </div>
+      </motion.div>
 
-export default PayToCard
+      <div className="mt-auto">
+        <FooterNav
+          nextTitle="Получить СМС-код"
+          nextClick={handleSubmit}
+          nextDisabled={card_num !== 16 || card_expire !== 4}
+          prevClick={() => navigate(-1)}
+        />
+      </div>
+    </>
+  );
+};
+
+export default PayToCard;
--- src/views/paymentOfFines/components/PaymentInfo.tsx
+++ src/views/paymentOfFines/components/PaymentInfo.tsx
@@ -1,68 +1,69 @@
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { ASSETS } from "../../../assets/images/assets";
-import {  FooterNav, Text } from "../../../components"
+import { FooterNav, Text } from "../../../components";
 import { useFormContext } from "../../../context/inputTypeContext";
 import { APP_ROUTES } from "../../../router";
 
-
 const PaymentInfo = () => {
-      // @ts-ignore
-      const { responseData } = useFormContext();
-      const navigate = useNavigate()
+    // @ts-ignore
+    const { responseData } = useFormContext();
+    const navigate = useNavigate();
+
     return (
         <>
-            <div className="bg-[#F4F4F4] p-[25px] rounded-[35px]">
+            <motion.div
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+                className="bg-[#F4F4F4] p-[25px] rounded-[35px]"
+            >
                 <div>
                     <p className="text-[36px]">Выберите способ оплаты:</p>
                 </div>
-               <div className="bg-white flex justify-between items-center mt-[20px] p-[15px] rounded-[12px]">
+                <div className="bg-white flex justify-between items-center mt-[20px] p-[15px] rounded-[12px]">
                     <img
                         src={responseData?.vendor?.logo}
                         className="min-w-[88px] w-[88px] h-[88px] rounded-full object-contain"
                         alt=""
                     />
-
                     <Text
                         text={`${responseData?.vendor?.name}`}
                         className="text-[25px] font-bold"
                     />
-               </div>
-                    <div className="grid grid-cols-[60%_1fr] gap-[20px] mt-[10px]">
-                         <div className="flex flex-col gap-[20px]  bg-white p-[15px] rounded-[12px]">
-                            {responseData?.vendor_info?.map((item: any, ind: any) => (
-                                <div key={ind} className="flex items-center">
+                </div>
+                <div className="grid grid-cols-[60%_1fr] gap-[20px] mt-[10px]">
+                    <div className="flex flex-col gap-[20px] bg-white p-[15px] rounded-[12px]">
+                        {responseData?.vendor_info?.map((item: any, ind: any) => (
+                            <div key={ind} className="flex items-center">
                                 <div className="text-[22px] font-[500]">{item.label}</div>
-                                <div className="text-[22px]  ml-auto text-right">
+                                <div className="text-[22px] ml-auto text-right">
                                     {item.value}
                                 </div>
-                                </div>
-                            ))}
-                        </div>
-                        <div className="flex flex-col gap-y-[10px]  ">
-                            <div onClick={() => navigate(`${APP_ROUTES.PAYMENT_OF_FINE}/${APP_ROUTES.PAYMENT_OF_FINE_CASH}`)} className="flex flex-col justify-center items-center h-full bg-white  rounded-[12px]">
-                                <p className="text-[25px]">Наличными</p>
-                                <img src={ASSETS.CashMoneyNew} alt="" />
                             </div>
-                            <div onClick={() => navigate(`${APP_ROUTES.PAY_TO_CARD}`)} className="flex flex-col justify-center items-center bg-white  h-full rounded-[12px]">
-                                <p className="text-[25px]">Банковской картой</p>
-                                <img src={ASSETS.Card} alt="" />
-                            </div>
-                        </div>
-
+                        ))}
                     </div>
-                   
-
-               
-            </div>
-            <FooterNav showNextButton={false} prevClick={() => navigate(-1)}/>
+                    <div className="flex flex-col gap-y-[10px]">
+                        <div
+                            onClick={() => navigate(`${APP_ROUTES.PAYMENT_OF_FINE}/${APP_ROUTES.PAYMENT_OF_FINE_CASH}`)}
+                            className="flex flex-col justify-center items-center h-full bg-white rounded-[12px] cursor-pointer"
+                        >
+                            <p className="text-[25px]">Наличными</p>
+                            <img src={ASSETS.CashMoneyNew} alt="" />
+                        </div>
+                        <div
+                            onClick={() => navigate(`${APP_ROUTES.PAY_TO_CARD}`)}
+                            className="flex flex-col justify-center items-center bg-white h-full rounded-[12px] cursor-pointer"
+                        >
+                            <p className="text-[25px]">Банковской картой</p>
+                            <img src={ASSETS.Card} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </motion.div>
+            <FooterNav showNextButton={false} prevClick={() => navigate(-1)} />
         </>
-    )
-}
+    );
+};
 
-export default  PaymentInfo
-
-
-
-
-
-
+export default PaymentInfo;
